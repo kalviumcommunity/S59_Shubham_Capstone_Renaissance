214b755cfd44de6f7a29b030c071ed790071fdd3
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-03 12:29:41 +0530

    Robust error handling for get

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -6,7 +6,8 @@ const getData = async (req, res) => {
         res.status(200).json(data)
     }
     catch (error) {
-        res.status(500).json({ message: error.message })
+        console.log('Error fetching data: ', error.message)
+        res.status(500).json({ message: "Failed to fetch. Could not find data" })
     }
 
 }
