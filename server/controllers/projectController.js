e6ed23b49d198acde7aaa909df2ae34d9936625d
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-03 12:23:03 +0530

    HTTP status code updated

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -3,7 +3,7 @@ const projectModel = require('../models/projectSchema')
 const getData = async (req, res) => {
     try {
         const data = await projectModel.find()
-        res.json(data)
+        res.status(200).json(data)
     }
     catch (error) {
         res.status(500).json({ message: error.message })
--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ const PORT = process.env.PORT || 8080
 const routes = require('./routes/projectRoutes')
 
 connectToDB()
+
 app.use(express.json())
 app.use('/api', routes)
+
 app.listen(PORT, () => {
     console.log("Listening at Port", PORT)
 })
