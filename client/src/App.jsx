1dd6c151d07920c48e916205acde10454bbd9313
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-10 15:26:23 +0530

    Landing Page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import SimpleSlider from './components/Slider'
+import Home from './components/Home'
+import './App.css'
 function App() {
   return (
-    <div>
-      Hi,This is the Capstone Project!
-    </div>
+    <>
+      <SimpleSlider />
+      <Home />
+    </>
   )
 }
 
