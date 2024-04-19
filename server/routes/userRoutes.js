8f9cf0bb997d18f518a901d468fe6715d8113515
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-19 09:58:15 +0530

    Reference between user and his projects added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, updateUser, deleteUser } = require('../controllers/userController')
+const { registerUser, updateUser, deleteUser, getUserProjects } = require('../controllers/userController')
 
 router.post('/register', registerUser)
 router.put('/update', updateUser)
 router.delete('/delete/:mail', deleteUser)
+router.get('/userProjects/:userId', getUserProjects)
 
 module.exports = router
