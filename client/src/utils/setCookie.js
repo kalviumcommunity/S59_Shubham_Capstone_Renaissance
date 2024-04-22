7b85c028262b9e6990caaad5ee8cfefbab3bba48
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-22 10:42:32 +0530

    Added URI encode component

--- client/src/utils/setCookie.js
+++ client/src/utils/setCookie.js
@@ -3,8 +3,9 @@ function setCookie(cookiename, cookieVal, expiryDays) {
     if (expiryDays) {
         let date = new Date()
         date.setTime(date.getTime() + (expiryDays * 24 * 60 * 60 * 1000))
-        expires = '; expires =' + date.toUTCString()
+        expires = '; expires = ' + date.toUTCString()
     }
+    cookieVal = encodeURIComponent(cookieVal)
     document.cookie = cookiename + "=" + cookieVal + expires + ";path=/"
 }
 
