90dd4fc47f3b748b672eb1d377006aab9f0f76be
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-22 10:31:57 +0530

    cookie added for user

--- client/src/utils/loginUtil.js
+++ client/src/utils/loginUtil.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
+import setCookie from './setCookie'
 
 const registerUtil = (data) => {
     axios.post('https://renaissance-server.onrender.com/user/login', data)
         .then(response => {
+            setCookie('user', response.data.username, 1)
             console.log("Login Successful: ", response.data)
         })
         .catch(error => {
---
+++ client/src/utils/setCookie.js
@@ -0,0 +1,11 @@
+function setCookie(cookiename, cookieVal, expiryDays) {
+    let expires = ""
+    if (expiryDays) {
+        let date = new Date()
+        date.setTime(date.getTime() + (expiryDays * 24 * 60 * 60 * 1000))
+        expires = '; expires =' + date.toUTCString()
+    }
+    document.cookie = cookiename + "=" + cookieVal + expires + ";path=/"
+}
+
+export default setCookie
