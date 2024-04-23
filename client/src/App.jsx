dc90d08c09c1d54abb44798e4c87a58bca0a632d
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-23 12:54:33 +0530

    Added a UI to add new project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from './components/Dashboard'
 import Register from './components/Register'
 import './index.css'
 import './App.css'
+import NewProject from './components/NewProject'
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/Register' element={<Register />} />
         <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/NewProject' element={<NewProject />} />
       </Routes>
     </>
   )
