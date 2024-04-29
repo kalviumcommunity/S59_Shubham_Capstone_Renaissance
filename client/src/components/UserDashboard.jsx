ddae70ed749b918680adda35ffc9af4d90abdfae
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-29 12:17:09 +0530

    Backend updated and bugs resolved

--- client/src/components/UserDashboard.jsx
+++ client/src/components/UserDashboard.jsx
@@ -19,17 +19,22 @@ function UserDashboard() {
                 console.log("Fetched Data: ", response.data)
             })
             .catch(error => {
-                console.log(error.response.data)
+                if (error.response) {
+                    console.log(error.response.data)
+                }
+                else {
+                    console.log("Some error occurred. Try Again Later", error)
+                }
             })
     }
     return (
         <div className='flex'>
             <div className="pt-20 shadow-xl h-[100vh] fixed w-[19%]">
                 <button className="bg-[#3F5F4F] ml-3 text-sm text-white px-2 py-1.5 rounded">+ New</button>
-                <div className='mt-2'>
+                <form className='mt-2'>
                     <input type="text" placeholder="Search Here" className="pl-10 py-3 text-sm w-[250px] my-5 absolute bg-gray-100 left-[-15px] h-[30px] rounded-3xl border-transparent" />
                     <button className="z-[10] absolute bg-[#3F5F4F] text-slate-100 border-solid border-[#3F5F4F] border-2 py-1.5 px-3 left-[200px] mt-[20px] rounded-3xl rounded-l-none" ><img src={searchIcon} className='w-[15px]' /></button>
-                </div>
+                </form>
                 <p className='ml-3 mt-20 text-base text-slate-800 font-semibold'>Your Contributions:</p>
                 <div className='m-3 w-[240px]'>
                     {userProjects && userProjects.map(ele => (
@@ -38,10 +43,10 @@ function UserDashboard() {
                 </div>
             </div>
             <div className='bg-[#F4F4F4] w-[81%] ml-[19%] px-10 pt-20'>
-                <div className='shadow-xl'>
+                <form className='shadow-xl'>
                     <input type="text" placeholder="Search Here" className="pl-[70px] py-1.5 text-[15px] w-[400px] absolute left-[230px] rounded-3xl border-transparent" />
                     <button className="z-[10] absolute bg-[#3F5F4F] border-solid border-[#3F5F4F] border-2 py-1.5 px-3 left-[600px]  rounded-3xl rounded-l-none" ><img src={searchIcon} className='w-[16.5px]' /></button>
-                </div>
+                </form>
                 <h1 className='font-bold text-3xl mt-[70px]'>Dashboard</h1>
                 <p className='mt-1.5 text-sm'>Explore new projects and create your own too!</p>
 
@@ -66,18 +71,18 @@ function UserDashboard() {
                 <button className="text-[#3F5F4F] font-semibold text-sm border-solid border-[#3F5F4F] mx-3 my-5 rounded border py-1.5 px-3" >Contribute</button>
                 <div className='flex flex-wrap'>
                     {userProjects.map(project => (
-                            <div className='bg-white m-3 rounded-xl w-[30%] px-5 py-8 shadow-lg flex flex-col justify-center'>
-                                <div className='flex'>
-                                    <img src={deBonaparte} alt="deBonaparte" className='rounded-full w-20 h-20 shadow-lg' />
-                                    <div className='p-3'>
-                                        <h1 className='font-bold text-lg'>Shubhh_Thakur</h1>
-                                        <p className='text-slate-700 text-[12px]'>Creative Writer, Author, Director</p>
-                                    </div>
+                        <div className='bg-white m-3 rounded-xl w-[30%] px-5 py-8 shadow-lg flex flex-col justify-center'>
+                            <div className='flex'>
+                                <img src={deBonaparte} alt="deBonaparte" className='rounded-full w-20 h-20 shadow-lg' />
+                                <div className='p-3'>
+                                    <h1 className='font-bold text-lg'>Shubhh_Thakur</h1>
+                                    <p className='text-slate-700 text-[12px]'>Creative Writer, Author, Director</p>
                                 </div>
-                                <h1 className='font-bold mt-5'>{project.title}</h1>
-                                <p className='text-slate-700 text-sm'>{project.description}</p>
                             </div>
-                        ))}
+                            <h1 className='font-bold mt-5'>{project.title}</h1>
+                            <p className='text-slate-700 text-sm'>{project.description}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
