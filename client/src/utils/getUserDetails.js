8685f7df62d9adff4edfc56cee111773fe244ff6
ShubhamThakur025 <thakur0805@gmail.com>
2024-05-06 12:15:38 +0530

    Worked over feedback

--- client/src/utils/getUserDetails.js
+++ client/src/utils/getUserDetails.js
@@ -1,10 +1,20 @@
 import getCookie from './getCookie'
-import {jwtDecode }from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 
 const getUserDetails = (query) => {
     const token = getCookie('accessToken')
-    const decodedToken =  jwtDecode(token)
-    return decodedToken[query]
+    if (!token) {
+        console.log("No Token available")
+        return null
+    }
+    try {
+        const decodedToken = jwtDecode(token);
+        return decodedToken[query];
+    }
+    catch (error) {
+        console.log('Failed to decode token', error);
+        return null;
+    }
 }
 
 export default getUserDetails
