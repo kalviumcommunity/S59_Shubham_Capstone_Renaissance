39a8fb1fe63493fcd5663949ad58b9531a353f8f
ShubhamThakur025 <thakur0805@gmail.com>
2024-05-09 11:34:46 +0530

    Worked over feedback

--- server/routes/pullRoutes.js
+++ server/routes/pullRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { setpull, getApprovalRequests, getOnepull, clearpull } = require('../controllers/pullsController')
 
-router.get('/get-/:pullID', getOnepull)
+router.get('/get-pull/:pullID', getOnepull)
 router.get('/requests/:userID', getApprovalRequests)
 router.post('/pull', setpull)
 router.delete('/clear-pull/:pullID', clearpull)
